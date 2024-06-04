05ec2bff3d53886a9d51272a030191abe7278a06
idalmoribeiro <idalmoribeiro6@hotmail.com>
2024-06-04 09:33:51 -0300

    Update script.js
    faltou um fechamento de parênteses na linha 12

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function playRockPaperScissor (player1, player2){
         return "Jogador 2 venceu!";
     }   else if  (((player1 == "Pedra") && (player2 == "Tesoura"))
         ||        ((player1 == "Papel") && (player2 == "Pedra"))
-        ||        ((player1 == "Tesoura") && (player2 == "Papel")){
+        ||        ((player1 == "Tesoura") && (player2 == "Papel"))){
         return "Jogador 1 venceu!";
     }  
     
