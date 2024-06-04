f84bb6cc35a59c41ed0843764eb7d5e626487525
idalmoribeiro <idalmoribeiro6@hotmail.com>
2024-06-04 09:24:33 -0300

    versao2 paper_rock_scissor
    coloquei todas as possibilidades de vitória de um jogador no mesmo operador condicional "if" usando o comparativo "ou"

--- script.js
+++ script.js
@@ -3,19 +3,15 @@ function playRockPaperScissor (player1, player2){
 
     if (player1 == player2){
         return "Empate!";
-    }   else if ((player1 == "Pedra") && (player2 == "Papel")){
+    }   else if (((player1 == "Pedra") && (player2 == "Papel")) 
+        ||       ((player1 == "Papel") && (player2 == "Tesoura"))
+        ||       ((player1 == "Tesoura") && (player2 == "Pedra"))){
         return "Jogador 2 venceu!";
-    }   else if ((player1 == "Pedra") && (player2 == "Tesoura")){
+    }   else if  (((player1 == "Pedra") && (player2 == "Tesoura"))
+        ||        ((player1 == "Papel") && (player2 == "Pedra"))
+        ||        ((player1 == "Tesoura") && (player2 == "Papel")){
         return "Jogador 1 venceu!";
-    }   else if ((player1 == "Papel") && (player2 == "Pedra")){
-        return "Jogador 1 venceu!";
-    }   else if ((player1 == "Papel") && (player2 == "Tesoura")){
-        return "Jogador 2 venceu!";
-    }   else if ((player1 == "Tesoura") && (player2 == "Pedra")){
-        return "Jogador 2 venceu!";
-    }   else if ((player1 == "Tesoura") && (player2 == "Papel")){
-        return "Jogador 1 venceu!";
-    }
+    }  
     
 }
 
